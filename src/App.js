8140ad1b10dc739d329efa9b02be2fa2cbcaed5b
Calvin Calvento <calvento.calvin@gmail.com>
2020-04-15 01:43:55 +0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import EventCalendar from "./EventCalendar";
@@ -6,28 +6,27 @@ import CssBaseline from "@material-ui/core/CssBaseline";
 import Typography from "@material-ui/core/Typography";
 import Container from "@material-ui/core/Container";
 import Grid from "@material-ui/core/Grid";
-import Paper from '@material-ui/core/Paper';
-import { makeStyles } from '@material-ui/core/styles';
-import TextField from '@material-ui/core/TextField';
-import Button from '@material-ui/core/Button';
-import axios from 'axios';
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-import Appbar from './AppBar'
+import Paper from "@material-ui/core/Paper";
+import { makeStyles } from "@material-ui/core/styles";
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
+import axios from "axios";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import Appbar from "./AppBar";
 toast.configure({
   autoClose: 2000,
   draggable: false,
   //etc you get the idea
 });
 function App() {
-
   const useStyles = makeStyles((theme) => ({
     root: {
       flexGrow: 1,
     },
     paper: {
       padding: theme.spacing(2),
-      textAlign: 'center',
+      textAlign: "center",
       color: theme.palette.text.secondary,
     },
     textField: {
@@ -38,23 +37,16 @@ function App() {
   }));
 
   const [eventForm, setEventForm] = useState({
-
     event: "",
     to: "",
-    from: ""
-
+    from: "",
   });
   function validate() {
-    if (
-      eventForm.event == "" ||
-      eventForm.to == "" ||
-      eventForm.from == "" 
-      
-    ) {
+    if (eventForm.event == "" || eventForm.to == "" || eventForm.from == "") {
       toast.error("Please input all fields");
       return false;
     }
-return true
+    return true;
   }
 
   function handleChange(event) {
@@ -62,85 +54,81 @@ return true
 
     setEventForm({ ...eventForm, [event.target.name]: value });
 
-    console.log(eventForm,'haha')
+    console.log(eventForm, "haha");
   }
 
-  function submitForm(){
+  function submitForm() {
     const isValidate = validate();
 
-    let from_date = new Date(eventForm.from)
-    let to_date = new Date(eventForm.to)
+    let from_date = new Date(eventForm.from);
+    let to_date = new Date(eventForm.to);
     console.log(from_date < to_date);
 
-
     if (isValidate == true) {
+      if (from_date <= to_date) {
+        axios
+          .post(`https://murmuring-retreat-33974.herokuapp.com/api/add-event`, {
+            eventForm,
+          })
+          .then((res) => {
+            if (res) {
+              toast.success("Success Adding Event");
 
-      if(from_date <= to_date){
-        axios.post(`https://murmuring-retreat-33974.herokuapp.com/api/add-event`, { eventForm })
-        .then(res => {
-          if(res){
-    
-            toast.success('Success Adding Event');
-    
-            window.location.replace('/')
-          }
-        })
-      }
-      else{
+              window.location.replace("/");
+            }
+          });
+      } else {
         toast.error("End date must be greater than start date");
-
       }
-  
+    }
   }
-  }
 
- 
-
-
-  const classes = useStyles()
+  const classes = useStyles();
   return (
     <div className="App">
       <Appbar />
 
       <Container maxWidth="lg">
-        <Grid container  spacing={0}>
-          <Grid item xs={12} md={6} lg={6}>
-
-          <TextField id="standard-basic" label="Event Name" name="event" style={{width : '80%'}}               onChange={handleChange}
- />
-    <br />
-    <br />
-          <TextField
-        id="date"
-        label="From"
-        type="date"
-        name='from'
-        className={classes.textField}
-        InputLabelProps={{
-          shrink: true,
-        }}
-        onChange={handleChange}
-
-      />
-        <TextField
-        id="date"
-        label="To"
-        type="date"
-        name='to'
-        className={classes.textField}
-        InputLabelProps={{
-          shrink: true,
-        }}
-        onChange={handleChange}
-
-      />
-  <br />
-  <br />
-      <Button variant="contained" color="primary" onClick={submitForm}>
-        Add Event
-</Button>
+        <Grid container spacing={0}>
+          <Grid item xs={12} md={6} lg={6} style={{padding: '10px'}}>
+            <TextField
+              id="standard-basic"
+              label="Event Name"
+              name="event"
+              style={{ width: "80%" }}
+              onChange={handleChange}
+            />
+            <br />
+            <br />
+            <TextField
+              id="date"
+              label="From"
+              type="date"
+              name="from"
+              className={classes.textField}
+              InputLabelProps={{
+                shrink: true,
+              }}
+              onChange={handleChange}
+            />
+            <TextField
+              id="date"
+              label="To"
+              type="date"
+              name="to"
+              className={classes.textField}
+              InputLabelProps={{
+                shrink: true,
+              }}
+              onChange={handleChange}
+            />
+            <br />
+            <br />
+            <Button variant="contained" color="primary" onClick={submitForm}>
+              Add Event
+            </Button>
           </Grid>
-          <Grid item xs={12} md={6} lg={6}>
+          <Grid item xs={12} md={6} lg={6} style={{padding: '10px'}}>
             <EventCalendar />
           </Grid>
         </Grid>
